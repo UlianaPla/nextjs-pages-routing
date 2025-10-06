a7017ebb85f9028cf24197c40683b5af25c42098
Ulia <lyanusia@gmail.com>
2025-10-06 16:57:28 +0200

    Navigating Programmatically

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -1,13 +1,23 @@
-import { useRouter } from 'next/router';
+import { useRouter } from "next/router";
 
 function ClientProjectsPage() {
   const router = useRouter();
 
   console.log(router.query); // Here is the id property
 
+  function loadProjectHandler() {
+    // load data...
+    // router.push('/clients/max/projecta') // option 1
+    router.push({
+      pathname: "/clients/[id]/[clientprojectid]",
+      query: { id: router.query.id, clientprojectid: "projecta" },
+    }); // option 2
+  }
+
   return (
     <div>
       <h1> The Projects of a Given Client</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   );
 }
